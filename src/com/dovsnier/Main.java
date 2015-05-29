d0c9052be53ad0cef9ed8982dc0be7ab19a371df
DovSnier <1305314861@qq.com>
2015-05-29 22:44:57 +0800

    the add main class

---
+++ src/com/dovsnier/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.dovsnier;
+
+/**
+ * <pre>
+ * Main
+ * </pre>
+ * 
+ * @author dovsnier
+ * @version 1.0.0
+ * @since jdk 1.7
+ */
+public class Main {
+	
+	/**
+	 * <br>
+	 * 2015年5月29日
+	 * 
+	 * @version 0.0.1
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+}
