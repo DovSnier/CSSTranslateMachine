4343b68cd49148a45aab0ed259dda44afad04649
DovSnier <1305314861@qq.com>
2015-05-29 23:36:11 +0800

    the add css type file

---
+++ src/com/dovsnier/interpreter/CssType.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.dovsnier.interpreter;
+
+/**
+ * <pre>
+ * CssType
+ * </pre>
+ * 
+ * @author dovsnier
+ * @version 1.0.0
+ * @since jdk 1.7
+ */
+public enum CssType {
+	CSS_DOCUMENT, CSS_ROOT, CSS_NODE
+}
