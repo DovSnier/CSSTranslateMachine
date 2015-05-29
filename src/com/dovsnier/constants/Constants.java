925fe2f122e9e47672d6f4b6d877892cbc78bd89
DovSnier <1305314861@qq.com>
2015-05-29 23:38:37 +0800

    the add path constants with the constants

---
+++ src/com/dovsnier/constants/Constants.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.dovsnier.constants;
+
+/**
+ * <pre>
+ * Constants
+ * </pre>
+ * 
+ * @author dovsnier
+ * @version 1.0.0
+ * @since jdk 1.7
+ */
+public final class Constants {
+	
+	/**
+	 * 
+	 * <pre>
+	 * PathConstants
+	 * </pre>
+	 * 
+	 * @author dovsnier
+	 * @version 1.0.0
+	 * @since jdk 1.7
+	 */
+	public static class PathConstants {
+		
+		/** the base configuration directory */
+		public static final String PATH_CONFIGURATION = "conf";
+		/** the base source css directory */
+		public static final String PATH_CSS = "css";
+		/** the base resolution css directory */
+		public static final String PATH_RESOLUTION_CSS = "resolution-css";
+	}
+}
