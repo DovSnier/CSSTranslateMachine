993008db5fb05e3487da4a3255f32b5305f19a0b
DovSnier <1305314861@qq.com>
2015-05-30 13:54:17 +0800

    the add css translate machine test environment

---
+++ src-test/com/dovsnier/interpreter/InterpreterTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.dovsnier.interpreter;
+
+import org.junit.Test;
+
+/**
+ * <pre>
+ * InterpreterTest
+ * </pre>
+ * 
+ * @author dovsnier
+ * @version 1.0.0
+ * @since jdk 1.7
+ * 
+ */
+public class InterpreterTest {
+	
+	private String path = "c:\\temp";
+	private String name = "nav.css";
+	
+	// private String expected = "";
+	/**
+	 * Test method for {@link com.dovsnier.interpreter.Interpreter#parseCssDocument(java.lang.String, java.lang.String)}.
+	 */
+	// @Test(expected = FileNotFoundException.class)
+	@Test
+	public void testParseCssDocument() {
+		Interpreter interpreter = new Interpreter();
+		interpreter.parseCssDocument(path, name);
+		// fail("Not yet implemented");
+	}
+}
