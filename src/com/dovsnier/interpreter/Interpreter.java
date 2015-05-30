7f8c873433cb45a23071bc79302afa77e505dcef
DovSnier <1305314861@qq.com>
2015-05-30 14:47:40 +0800

    the impl sample parse css style sheet function that maybe a lot have bug and so on.

--- src/com/dovsnier/interpreter/Interpreter.java
+++ src/com/dovsnier/interpreter/Interpreter.java
@@ -11,7 +11,9 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 
+import com.dovsnier.bean.Attribute;
 import com.dovsnier.bean.DocumentBean;
+import com.dovsnier.bean.NodeBean;
 import com.dovsnier.bean.RootNodeBean;
 
 /**
@@ -43,26 +45,51 @@ public class Interpreter extends AbstractInterpreter {
 	public void parseCssDocument(String path, String name) {
 		String absolutePath = detectionParameterSuffix(path);
 		String alias = name;
-		RootNodeBean rootNode;
 		File file = new File(absolutePath, name);
 		DocumentBean document = null;
+		RootNodeBean rootNode = null;
+		NodeBean node = null;
 		try {
 			if (!file.exists()) {
 				String msg = "the current " + absolutePath + name + " is not found exception.";
 				throw new FileNotFoundException(msg);
 			} else {
 				document = new DocumentBean();
-				document.setAlias(alias.substring(0, alias.lastIndexOf(".")));
 				rootNode = new RootNodeBean();
+				document.setAlias(alias.substring(0, alias.lastIndexOf(".")));
 				document.setRootNode(rootNode);
 			}
 			reader = new FileReader(file);
 			br = new BufferedReader(reader);
-			StringBuffer sb = new StringBuffer();
 			String lineData = "";
+			StringBuffer sbCache = new StringBuffer();
 			// TODO the handle parse css style sheet
 			while (null != (lineData = br.readLine())) {
-				sb.append(lineData);
+				if ("".equalsIgnoreCase(lineData)) { // TODO the current line is empty line that belong to text format category
+				} else if (lineData.contains("{") && lineData.contains("}")) { // TODO the current line exist node bean with one or more, and need careful analysis
+				} else if (lineData.contains("{") && !lineData.contains("}")) { // TODO the current line exist node bean that is not entire,and need continue ready
+					node = new NodeBean();
+					String nodeName = lineData;
+					nodeName = nodeName.replace("{", "").trim();
+					node.setNodeName(nodeName);
+				} else if (!lineData.contains("{") && lineData.contains("}")) {// TODO the current line exist node bean that maybe is node bean ended symbol or item node bean ended symbol
+					rootNode.getRootNode().add(node);// the add node to root node or not to item node
+					// the add node to item node or not to root node
+				} else if (!lineData.contains("{") && !lineData.contains("}")) {// TODO the current line exist node bean that maybe is node bean attribute or item node bean attribute
+					if (lineData.contains(";")) { // these maybe is node bean attribute or item node bean attribute
+						Attribute attribute = new Attribute();
+						String kvPair = lineData;
+						kvPair = kvPair.replace(";", "").trim();
+						String[] kvContainer = kvPair.split(":");
+						if (null != kvContainer) {
+							attribute.setKey(kvContainer[0].trim());
+							attribute.setValue(kvContainer[1].trim());
+						} else {
+						}
+						node.getAttribute().add(attribute);
+					}
+				}
+				sbCache.append(lineData);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
